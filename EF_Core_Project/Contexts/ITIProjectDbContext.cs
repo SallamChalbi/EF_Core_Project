aecc5fd8bc63fe2ad87f2ba8707dce1e33239002
SallamChalbi <139037169+SallamChalbi@users.noreply.github.com>
2026-03-15 18:07:08 +0100

    Add DbContext (Install Microsoft.EntityFrameworkCore.SqlServer v6.0.36)

---
+++ EF_Core_Project/Contexts/ITIProjectDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EF_Core_Project.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_Project.Contexts
+{
+    internal class ITIProjectDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=.; Database=ITIProject; Trusted_Connection=True");
+
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Topic> Topic { get; set; }
+    }
+}
